1d0fecded989b2433a608a4f5074d38116dac5a6
Darko <qq1572055493@icloud.com>
2018-07-03 15:40:40 +0800

    Initial Commit

---
+++ SummerHolidayProject/SummerHolidayProject-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SummerHolidayProject-Bridging-Header.h
+//  SummerHolidayProject
+//
+//  Created by Darko on 2018/7/3.
+//  Copyright © 2018年 Darko. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
